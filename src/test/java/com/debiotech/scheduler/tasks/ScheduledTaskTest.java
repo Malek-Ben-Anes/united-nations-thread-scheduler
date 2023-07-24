02af6246d934541c09ce1fa135bfdf041de4e06b
Malik <benanesmalek@gmail.com>
2023-07-24 05:24:13 +0200

    "first commit"

---
+++ src/test/java/com/debiotech/scheduler/tasks/ScheduledTaskTest.java
@@ -0,0 +1,14 @@
+package com.debiotech.scheduler.tasks;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ScheduledTaskTest {
+
+    @Test
+    void hello() {
+        assertEquals(0, 0);
+    }
+
+}
